9fff9629af1ed40791128505c6a72f1d7aaf280f
poc-sleepy <sleepy.sleepy.days@gmail.com>
2023-07-11 22:50:01 +0900

    ✨ StyledButtonの新規追加

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,5 +1,12 @@
+import { StyledButton } from '@/components/StyledButton';
+
 const Sample = () => {
-  return <span>さんぷるぺえじ</span>;
+  return (
+    <>
+      <span>さんぷるぺえじ</span>
+      <StyledButton variant="primary">すたいるぼたん</StyledButton>
+    </>
+  );
 };
 
 export default Sample;
